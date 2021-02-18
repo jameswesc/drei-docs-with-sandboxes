ba06e445d6cc2ab1686fb5ccd440ce8913f245b5
James Gregory <j.gregory@smashdelta.com>
2021-02-18 17:40:47 +1100

    add ico example

---
+++ src/sandboxes/ico/Example.tsx
@@ -0,0 +1,24 @@
+import { Icosahedron } from '@react-three/drei'
+import React, { useRef } from 'react'
+import { useFrame } from 'react-three-fiber'
+import { Mesh } from 'three'
+
+type Props = {}
+
+export default function Example({}: Props) {
+  const ref = useRef<Mesh>(null)
+
+  useFrame(() => {
+    if (ref.current) {
+      ref.current.rotation.x = ref.current.rotation.y = ref.current.rotation.z += 0.01
+    }
+  })
+
+  return (
+    <group>
+      <Icosahedron ref={ref}>
+        <meshNormalMaterial flatShading />
+      </Icosahedron>
+    </group>
+  )
+}
